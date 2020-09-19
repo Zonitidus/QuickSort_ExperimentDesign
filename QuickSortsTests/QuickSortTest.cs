5949ea77cfc737ecacd8e9546bc7731ee2acbc71
Juan Ossa <juan.ossa@hotmail.com>
2020-09-19 17:50:50 -0500

    First test in type Int32 is green

--- QuickSortsTests/QuickSortTest.cs
+++ QuickSortsTests/QuickSortTest.cs
@@ -10,12 +10,20 @@ namespace QuickSortsTests
     public class QuickSortTest
     {
         //private QuickSort<string> quick;
-        private void SmallScene (){
+        private QuickSort<Int32> SmallInt32Scene (){
             QuickSort<Int32> quick = new QuickSort<Int32>();
+            return quick;
         }
         [TestMethod]
-        public void TestMethod1()
+        public void TestSortingInInt32()
         {
+            Int32[] tsOrder = {0,2,4,6,9,86};
+            Int32[] ts = {4,6,86,9,0,2};
+            SmallInt32Scene().QuickSortAlgorithm(ts,0,5);
+            string resul1 = string.Join(".", tsOrder);
+            string resul2 = string.Join(".", ts);
+            Console.WriteLine(ts);
+            Assert.AreEqual(resul1,resul2);
         }
     }
 }
