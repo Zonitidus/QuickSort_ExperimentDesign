8db9b8d6b091942f2a0f2a30c41bbacd14d6e290
Juan Ossa <juan.ossa@hotmail.com>
2020-09-19 17:17:06 -0500

    Creating first generic scene

--- QuickSortsTests/QuickSortTest.cs
+++ QuickSortsTests/QuickSortTest.cs
@@ -2,15 +2,16 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using QuickSort_ExperimentDesign_Randomized_NotRandomized_;
 using QuickSort_ExperimentDesign_Randomized_NotRandomized_.SortingAlgorithms;
+using System.Collections.Generic;
 
 namespace QuickSortsTests
 {
     [TestClass]
     public class QuickSortTest
     {
-        private QuickSort<string> quick;
+        //private QuickSort<string> quick;
         private void SmallScene (){
-            //QuickSort quick = new QuickSort();
+            QuickSort<Int32> quick = new QuickSort<Int32>();
         }
         [TestMethod]
         public void TestMethod1()
