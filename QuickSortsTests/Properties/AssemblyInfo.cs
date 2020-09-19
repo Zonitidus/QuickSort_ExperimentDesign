ace034ad06fbb21161dd0e77f4074ed9d0c591a6
Juan Ossa <juan.ossa@hotmail.com>
2020-09-19 16:14:28 -0500

    Test Class Created

---
+++ QuickSortsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("QuickSortsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("QuickSortsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("62e3f71e-4cd4-4e88-8e09-456398a9cd02")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QuickSortsTests/QuickSortTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSort_ExperimentDesign_Randomized_NotRandomized_;
+using QuickSort_ExperimentDesign_Randomized_NotRandomized_.SortingAlgorithms;
+
+namespace QuickSortsTests
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        private QuickSort<string> quick;
+        private void SmallScene (){
+            //QuickSort quick = new QuickSort();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
