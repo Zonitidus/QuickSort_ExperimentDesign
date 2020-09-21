95f11254f725afafd46fde1b37cbf0d9463e85ad
Zonitidus <zonitidus@gmail.com>
2020-09-20 21:07:31 -0500

    Merging

---
+++ QuickSort_ExperimentDesign(Randomized(NotRandomized)/Experiment/Experiment.cs
@@ -0,0 +1,36 @@
+﻿using QuickSort_ExperimentDesign_Randomized_NotRandomized_.SortingAlgorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort_ExperimentDesign_Randomized_NotRandomized_.Experiment
+{
+    class Experiment
+    {
+        private QuickSort<Int32> sorter;
+        private Int32[] tenDataItems;
+        private Int32[] oneHundredDataItems;
+        private Int32[] oneThousandDataItems;
+        private Int32[] tenThousandDataItems;
+        private Int32[] oneHundredThousandItems;
+
+        public Experiment() {
+            this.tenDataItems = new Int32[10];
+            this.oneHundredDataItems = new Int32[100];
+            this.oneThousandDataItems = new Int32[1000];
+            this.tenThousandDataItems = new Int32[10000];
+            this.oneHundredThousandItems = new Int32[100000];
+            initArrays();
+        }
+
+        public void initArrays() {
+
+            for (int i = 0; i < tenDataItems.Length; i++) { 
+                
+            }
+        }
+
+    }
+}
